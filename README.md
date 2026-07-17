c4a0a9c7fa74606f21e5269664c69cf1bb7fddcd
Mobius Kyle <mobiuskyle@gmail.com>
2026-07-17 14:59:31 +0300

    Adding my README file and updating my progress on Python string methods

---
+++ README.md
@@ -0,0 +1,22 @@
+##Learning Python
+
+## Progress 
+This repository documents my journey learning Python — from the basics through to more advanced topics. I'm using it both as a study log and as a growing portfolio
+
+
+- [x] Basics (variables, data types, operators)
+- [x] String methods
+- [ ] Control flow (if/else, loops)
+- [ ] Functions
+- [ ] Data structures (lists, dicts, tuples, sets)
+- [ ] File handling
+- [ ] OOP basics
+- [ ] Mini projects
+
+## Structure
+
+Exercises are organized by topic as I learn them.
+
+## About me
+
+I'm learning Python as a beginner, documenting my progress publicly to stay accountable and build toward a portfolio.
--- strings.py
+++ strings.py
@@ -8,4 +8,19 @@ print(len(msg))
 #strings methods
 Course = 'Learning Python'
 #to use string methods we make use of the .(dot) operator
-#the len() method is used to
+#the len() method is used todisplay the number of items in a list.
+print(len(Course))
+
+"""to change the case of a string to either lowercase or uppercase and vice versa
+we use the .upper() and .lower() functions"""
+print(Course.upper())
+print(Course.lower())
+
+"""the .find() function is used to return the index/position of the first 
+occurrence of the defined character.The method is case sensitive"""
+print(Course.find('P'))
+
+#replace() method 
+"""The replace() method is used to replace a certain word or character with another"""
+print(Course.replace('Python' , 'Java'))
+print(Course.replace('P','J'))
