f54417713c7da4ca16c7bb4d73265f49b41258e1
Mobius Kyle <mobiuskyle@gmail.com>
2026-07-18 13:01:36 +0300

    adding arithmetic operations in python file that contains exercise and examples of arithmetic operations using python

---
+++ arithmetic_operations.py
@@ -0,0 +1,28 @@
+"""Arithmetic Operations supported in Python include:
+i)Addition
+ii)Subtraction
+iii)Multiplication
+iv)Division
+v)Modulus
+vi)Exponential
+vii)increment and decrement
+
+"""
+x=10
+y=3
+print(x+y) #Addition
+print(x-y)#Subtraction
+print(x*y)#Multiplication
+print(x/y)#Division - returns the value as it is 
+print(x//y)#Division - returns the whole value
+print(x%y)#Modulus - returns the remainder
+#x=x+3 can also be written as x+=3
+x+=3 #increment
+print(x)
+#y=y-1 can also be written as y-=1
+y-=1 #Decrement
+print(y)
+print(x**y)#exponential
+
+
+
