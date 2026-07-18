1eb6fa1b1f7ba7dcf0dee76df259c37d7f9cebed
Mobius Kyle <mobiuskyle@gmail.com>
2026-07-18 13:28:43 +0300

    add math_functions.py file with mathematical functions and relevant examples

---
+++ math_function.py
@@ -0,0 +1,18 @@
+x=2.9
+x1=round(2.9)
+#round() function rounds the number/variable to the nearest whole number
+print(x1)
+y=-5.4
+y1=abs(y)
+#abs - shortform of absolute.The abs() function returns the positive representation of the value
+print(y1)
+
+"""Python has an inbuilt module that contains a lot of mathematical
+operations.So one can just call the whole module or import it
+if need be.
+"""
+#Example:
+import math#read documentation for further clarification
+x=2.9
+x1=math.ceil(x)
+print(x1)
