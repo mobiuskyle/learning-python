c97ba289c568353bf4e80721902a1a58e908fef9
Mobius Kyle <mobiuskyle@gmail.com>
2026-07-25 11:49:35 +0300

    adding a simple task one that asks the user of their age and name

---
+++ practice_exercises.py
@@ -0,0 +1,6 @@
+#variables,input and type conversion
+## exercise 1
+name=input('What is your name? ')
+age=input('What is your age? ')
+nextAge = int(age)+1
+print(f"Hello [{name}] next year you'll be [{nextAge}]")
