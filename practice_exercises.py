2e91874eab97a34da8870cced187aba60a45c4cc
Mobius Kyle <mobiuskyle@gmail.com>
2026-07-25 13:29:25 +0300

    Adding task 4 that puts in practice the len() method and the comaprison of two characters to determine if the first letter of the inputed sentence is in uppercase or lowercase.Have also made use of the .upper() method.

--- practice_exercises.py
+++ practice_exercises.py
@@ -1,4 +1,4 @@
-"variables,input and type conversion"
+""""variables,input and type conversion"
 ## exercise 1
 name=input('What is your name? ')
 age=input('What is your age? ')
@@ -11,7 +11,17 @@ temperatureFahr = int(temperatureCelcius)*(9/5) + 32
 print(f'The temperature is {temperatureFahr} Fahrenheits')
 
 "Strings , Formatted Strings and String Methods"
+## exercise 3 
 full_name = input('What is your Full name? ')
 print(full_name.lower())
 print(full_name.upper())
-print(full_name[::-1])
+print(full_name[::-1])"""
+
+## exercise 4
+sentence = input('Write any random sentence of your choice : ')
+sentence_characters = len(sentence)
+print(f'this sentence has {sentence_characters} characters')
+if sentence[0] == sentence[0].upper():
+    print('the first letter is in capital')
+else:
+    print('the first letter is in lowercase')
