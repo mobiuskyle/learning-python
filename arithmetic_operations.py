b6c9b419c8c9cf2737c71b3a7f4f31a96b4145a7
Mobius Kyle <mobiuskyle@gmail.com>
2026-07-25 11:39:30 +0300

    update arithmetic_operations.py with a simple calculator program

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -24,5 +24,24 @@ y-=1 #Decrement
 print(y)
 print(x**y)#exponential
 
+"""Task
+Ask the user for two values a and b then ,Add code to print three lines where:
 
+i)The first line contains the sum of the two numbers.
+ii)The second line contains the difference of the two numbers (first - second).
+iii)The third line contains the product of the two numbers.
+iv)the fourth line contains the division of the two numbers 
+v)the fifth line contains the float division of the two numbers"""
 
+a=int(input("What is the value of a :"))
+b=float(input("What is the value of b :"))
+sum=a+b
+difference = a-b
+product=a*b
+division = a/b
+floatDivision = a//b
+print("the sum of a and b is : ",sum)
+print("the difference of a and b is : ",difference)
+print("the product af a and b is : ",product)
+print("the division of a and b is : ",division)
+print("the float division of a and b is : ",floatDivision)
