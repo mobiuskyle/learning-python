d62d4510c0c41861bda1616b33779abb709682e3
Mobius Kyle <mobiuskyle@gmail.com>
2026-07-25 12:04:15 +0300

    adding a simple task that converts the temperature in celcius to Fahrenheit

--- practice_exercises.py
+++ practice_exercises.py
@@ -4,3 +4,9 @@ name=input('What is your name? ')
 age=input('What is your age? ')
 nextAge = int(age)+1
 print(f"Hello [{name}] next year you'll be [{nextAge}]")
+
+## exercise 2
+temperatureCelcius = input('What is the current temperature? ')
+temperatureFahr = int(temperatureCelcius)*(9/5) + 32
+print(f'The temperature is {temperatureFahr} Fahrenheits')
+
